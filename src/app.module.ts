b9d00d014630c8d228444a06ea2a5e6ff77a1ed5
mohaned benmansour <mohaned.benmansour@esprit.tn>
2020-08-10 14:19:11 +0100

    fixing errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PostService } from './post/post.service';
 import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -17,7 +16,7 @@ import { join } from 'path';
       'mongodb+srv://mohaned:98898685968996@cluster0-hsoop.azure.mongodb.net/emaily?retryWrites=true&w=majority',
     ),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
